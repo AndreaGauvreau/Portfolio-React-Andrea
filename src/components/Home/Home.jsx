60ad9efc2b52ec2ef6fbea861ffe4a07f700d77c
Andrea.Gauvreau <andrea@teachizi.fr>
2023-02-19 19:10:35 +0100

    responsive begginings

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,18 +2,18 @@ import {Box} from '@chakra-ui/react'
 import React from 'react'
 import Main from './Main/Main'
 import Competences from './Competences/Competences'
-import CardMap from '../Home/Projects/floatingCards/CardsContent'
 import Menu from './Menu/Menu'
 import Contact from './Contact/Contact'
+import Project from './Projects/Project'
 
 export default function Home() {
   return (
     <>
-      <Box bgColor="#0d0d12" padding={0} m={0}>
+      <Box bgColor="#0d0d12" padding={0} m={0} w={'100%'} overflow={'hidden'}>
         <Menu />
         <Main />
         <Competences />
-        <CardMap />
+        <Project />
         <Contact />
       </Box>
     </>
--- src/components/Home/Projects/floatingCards/FakeData.jsx
+++ src/components/Home/Projects/floatingCards/FakeData.jsx
@@ -3,7 +3,7 @@ const sports = [
     id: 1,
     title: 'Teachizi.fr',
     description:
-      'Le football est un sport collectif qui oppose deux équipes de onze joueurs qui tentent de marquer des buts.',
+      "j'ai developpé la marketplace teachizi.fr dans le but d'obtenir un espace utilisateur eleve et professeur, possibilité de réserver et payer le cours d'un prof",
     categories: ['CMS', 'Marketplace', 'Mangopay'],
     image: 'https://i.ytimg.com/vi/DpjXFckjmp8/maxresdefault.jpg',
   },
@@ -11,7 +11,7 @@ const sports = [
     id: 2,
     title: 'Teachizi.hu',
     description:
-      'Le basketball est un sport d’équipe qui oppose deux équipes de cinq joueurs qui se disputent un ballon en dribblant, en passant et en marquant des paniers.',
+      "j'ai developpé la marketplace teachizi.fr dans le but d'obtenir un espace utilisateur eleve et professeur, possibilité de réserver et payer le cours d'un prof",
     categories: ['CMS', 'Marketplace', 'Mangopay'],
     image: 'https://i.ytimg.com/vi/DpjXFckjmp8/maxresdefault.jpg',
   },
@@ -19,7 +19,7 @@ const sports = [
     id: 3,
     title: 'Netflix',
     description:
-      'Le tennis est un sport de raquette qui oppose deux ou quatre joueurs qui se renvoient une balle en la frappant avec une raquette.',
+      "J'ai reproduit l'application netflix. j'ai utilisé principalement reactQuery et material ui afin d'avoir un une webapp à l'image de Netflix",
     categories: ['React', 'Material Ui', 'React Query'],
     image: 'https://i.ytimg.com/vi/ZMak63mHq5Y/maxresdefault.jpg',
   },
@@ -27,7 +27,7 @@ const sports = [
     id: 4,
     title: 'LaTeamV2',
     description:
-      'L’athlétisme est un ensemble de disciplines sportives individuelles qui regroupent des épreuves de course, de saut et de lancer.',
+      "J'ai contribué à l'amélioration de lateamV2, je me suis principalement occupé de divers problème ui/ux, appels API avec react Query",
     categories: ['React', 'React Query', 'Chakra Ui'],
     image:
       'https://camo.githubusercontent.com/a8f540f11243d7ecc371e7e8e469f8e139104f25576c1856113ad7d7f1f7346d/68747470733a2f2f6d696b65636f646575722e636f6d2f6d696b652f6173736574732f636f75727365732f72656163742d6d6173746572792d332e706e67',
@@ -36,7 +36,7 @@ const sports = [
     id: 5,
     title: 'Tracker de temps',
     description:
-      'La natation est un sport qui se pratique dans l’eau et qui regroupe des épreuves de nage libre, de dos, de brasse et de papillon.',
+      "J'ai contribué à l'amélioration de lateamV2, je me suis principalement occupé de divers problème ui/ux, appels API avec react Query",
     categories: ['React'],
     image:
       'https://images.unsplash.com/photo-1560090995-01632a28895b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2069&q=80',
@@ -45,7 +45,7 @@ const sports = [
     id: 6,
     title: 'Formizi',
     description:
-      'L’escalade est un sport qui consiste à gravir des parois rocheuses ou artificielles en utilisant des techniques d’escalade et de sécurité.',
+      "J'ai developpé Formizi qui est une lxp offrant une experience de formation digital innovante.",
     categories: ['CMS', 'LXP', 'LMS'],
     image: 'https://i.ytimg.com/vi/DpjXFckjmp8/maxresdefault.jpg',
   },
@@ -53,7 +53,7 @@ const sports = [
     id: 7,
     title: 'LaTeamV2',
     description:
-      'Le ski est un sport de glisse qui consiste à descendre des pistes enneigées en utilisant des skis, des bâtons et des bottes.',
+      "J'ai contribué à l'amélioration de lateamV2, je me suis principalement occupé de divers problème ui/ux, appels API avec react Query",
     categories: ['React', 'React Query', 'Chakra Ui'],
     image:
       'https://camo.githubusercontent.com/a8f540f11243d7ecc371e7e8e469f8e139104f25576c1856113ad7d7f1f7346d/68747470733a2f2f6d696b65636f646575722e636f6d2f6d696b652f6173736574732f636f75727365732f72656163742d6d6173746572792d332e706e67',
@@ -62,7 +62,7 @@ const sports = [
     id: 8,
     title: 'React',
     description:
-      'Le ski est un sport de glisse qui consiste à descendre des pistes enneigées en utilisant des skis, des bâtons et des bottes.',
+      "J'ai reproduit l'application netflix. j'ai utilisé principalement reactQuery et material ui afin d'avoir un une webapp à l'image de Netflix",
     categories: ['React', 'React Query', 'Chakra Ui'],
     image:
       'https://images.unsplash.com/photo-1565992441121-4367c2967103?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=654&q=80',
@@ -71,7 +71,7 @@ const sports = [
     id: 9,
     title: 'Netflix',
     description:
-      'Le ski est un sport de glisse qui consiste à descendre des pistes enneigées en utilisant des skis, des bâtons et des bottes.',
+      "J'ai reproduit l'application netflix. j'ai utilisé principalement reactQuery et material ui afin d'avoir un une webapp à l'image de Netflix",
     categories: ['React', 'Material Ui', 'React Query'],
     image: 'https://i.ytimg.com/vi/ZMak63mHq5Y/maxresdefault.jpg',
   },
